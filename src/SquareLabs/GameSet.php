130dc87d700f5d237018c926601f36bff456393a
Victor Farazdagi <simple.square@gmail.com>
2014-05-10 18:22:29 +0400

    Setup tests

---
+++ src/SquareLabs/GameSet.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SquareLabs;
+
+
+class GameSet
+{
+
+} 
---
+++ tests/GameSetTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class GameSetTest extends PHPUnit_Framework_TestCase
+{
+
+    public function  testPing()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
+ 
